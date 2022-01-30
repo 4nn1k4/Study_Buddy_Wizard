c5c1c9958b864ee72b91e2d3c11825e09c0e0810
4nn1k4 <annika.koehler21@gmail.com>
2022-01-30 14:52:26 +0100

    Updated_user_initiative_buttons

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 var Config = {
     INTRO_SYSTEM: "Hi, nice to meet you",
-    INTRO_USER: "Intro U",
+    INTRO_USER: "Hey, I'm your Study Buddy, I can help you while learning your vocabulary. You can ask for my functions if you need help. Good Luck!",
     OPTIONS_SYTEM: "o s",
-    OPTIONS_USER: "o u",
+    OPTIONS_USER: "You can ask me to pronounce the word, to build a sentence with the word, to explain the meaning of the word, and ask for hints for a better understanding of the word. If you're done, say, goodbye Study Buddy",
     CAN_I_HELP: "How can I help you?",
     NOT_DEFINED: "Sorry, I did not understand, please try again or ask for my functions",
     FURTHER_HELP: "Please ask, if you need help",
@@ -11,13 +11,8 @@ var Config = {
     REQUEST_PRONUNCIATION: "Please pronounce the word",
     NEXT_WORD: "the correct answer is, ",
     CORRECT: "correct",
-    INCORRECT: "incorrect, you can try again or ask for an hint",
-    OUTRO: "good bye"
+    INCORRECT: "incorrect, you can try again or ask for a hint",
+    OUTRO: "goodbye"
 };
 
-/**
- * evtl button incorrect in aufteilen & 2 Buttons draus machen:
- * 1) incorrect: Soll ich die Vokabel hinten anstellen und später darauf zurückkomen?
- * 2) incorrect: ich glaube, du benötigst noch etwas Hilfe bei diesem Wort. Soll ich dir auf eine andere Art & Weise bei dem Wort helfen?
- */
 export default Config;
